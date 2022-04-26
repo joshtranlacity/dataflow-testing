1a0e2bbbbf4b6e6fe9ae711271891a867dfb240b
josh <josh>
2022-04-26 14:44:33 -0700

    testing

--- tutorial.py
+++ tutorial.py
@@ -1,19 +1,19 @@
 # https://cloud.google.com/dataflow/docs/guides/templates/creating-templates
- class WordcountOptions(PipelineOptions):
+class WordcountOptions(PipelineOptions):
     @classmethod
     def _add_argparse_args(cls, parser):
-      # Use add_value_provider_argument for arguments to be templatable
-      # Use add_argument as usual for non-templatable arguments
-      parser.add_value_provider_argument(
-          '--input',
-          default='gs://dataflow-samples/shakespeare/kinglear.txt',
-          help='Path of the file to read from')
-      parser.add_argument(
-          '--output',
-          required=True,
-          help='Output file to write results to.')
-  pipeline_options = PipelineOptions(['--output', 'some/output_path'])
-  p = beam.Pipeline(options=pipeline_options)
+        # Use add_value_provider_argument for arguments to be templatable
+        # Use add_argument as usual for non-templatable arguments
+        parser.add_value_provider_argument(
+            '--input',
+            default='gs://dataflow-samples/shakespeare/kinglear.txt',
+            help='Path of the file to read from')
+        parser.add_argument(
+            '--output',
+            required=True,
+            help='Output file to write results to.')
+    pipeline_options = PipelineOptions(['--output', 'some/output_path'])
+    p = beam.Pipeline(options=pipeline_options)
 
-  wordcount_options = pipeline_options.view_as(WordcountOptions)
-  lines = p | 'read' >> ReadFromText(wordcount_options.input)
+    wordcount_options = pipeline_options.view_as(WordcountOptions)
+    lines = p | 'read' >> ReadFromText(wordcount_options.input)
