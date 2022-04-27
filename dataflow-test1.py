6c970a1023b14457e20966fdc4b1f773b8c0f6b8
Josh Tran <josh.tran@lacity.org>
2022-04-27 10:45:07 -0700

    test runtime

--- dataflow-test1.py
+++ dataflow-test1.py
@@ -39,63 +39,76 @@ from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.options.value_provider import StaticValueProvider
 from apache_beam.options.value_provider import RuntimeValueProvider
 
 
 class WordExtractingDoFn(beam.DoFn):
-  """Parse each line of input text into words."""
-  def process(self, element):
-    """Returns an iterator over the words of this element.
-    The element is a line of text.  If the line is blank, note that, too.
-    Args:
-      element: the element being processed
-    Returns:
-      The processed element.
-    """
-    return re.findall(r'[\w\']+', element, re.UNICODE)
+    """Parse each line of input text into words."""
+    def process(self, element):
+        """Returns an iterator over the words of this element.
+        The element is a line of text.  If the line is blank, note that, too.
+        Args:
+        element: the element being processed
+        Returns:
+        The processed element.
+        """
+        return re.findall(r'[\w\']+', element, re.UNICODE)
 
+
+class WordcountOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_value_provider_argument(
+            '--input',
+            default='gs://dataflow-samples/shakespeare/kinglear.txt',
+            help='Path of the file to read from')
+        parser.add_argument(
+            '--output',
+            required=True,
+            help='Output file to write results to.')
 
 def run(argv=None, save_main_session=True):
-  """Main entry point; defines and runs the wordcount pipeline."""
-  parser = argparse.ArgumentParser()
-  parser.add_value_provider_argument(
-      '--input',
-      dest='input',
-      default='gs://dataflow-samples/shakespeare/kinglear.txt',
-      help='Input file to process.')
-  parser.add_value_provider_argument(
-      '--output',
-      dest='output',
-      required=True,
-      help='Output file to write results to.')
-  known_args, pipeline_args = parser.parse_known_args(argv)
+    """Main entry point; defines and runs the wordcount pipeline."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input',
+        dest='input',
+        default='gs://dataflow-samples/shakespeare/kinglear.txt',
+        help='Input file to process.')
+    parser.add_argument(
+        '--output',
+        dest='output',
+        required=True,
+        help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
 
-  # We use the save_main_session option because one or more DoFn's in this
-  # workflow rely on global context (e.g., a module imported at module level).
-  pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
-  # The pipeline will be run on exiting the with block.
-  with beam.Pipeline(options=pipeline_options) as p:
+    # The pipeline will be run on exiting the with block.
+    with beam.Pipeline(options=pipeline_options) as p:
 
-    # Read the text file[pattern] into a PCollection.
-    lines = p | 'Read' >> ReadFromText(known_args.input)
+        # Read the text file[pattern] into a PCollection.
+        lines = p | 'Read' >> ReadFromText(known_args.input)
 
-    counts = (
-        lines
-        | 'Split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(str))
-        | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
-        | 'GroupAndSum' >> beam.CombinePerKey(sum))
+        counts = (
+            lines
+            | 'Split' >> (beam.ParDo(WordExtractingDoFn()).with_output_types(str))
+            | 'PairWithOne' >> beam.Map(lambda x: (x, 1))
+            | 'GroupAndSum' >> beam.CombinePerKey(sum))
 
-    # Format the counts into a PCollection of strings.
-    def format_result(word, count):
-      return '%s: %d' % (word, count)
+        # Format the counts into a PCollection of strings.
+        def format_result(word, count):
+            return '%s: %d' % (word, count)
 
-    output = counts | 'Format' >> beam.MapTuple(format_result)
+        output = counts | 'Format' >> beam.MapTuple(format_result)
 
-    # Write the output using a "Write" transform that has side effects.
-    # pylint: disable=expression-not-assigned
-    output | 'Write' >> WriteToText(known_args.output)
+        # Write the output using a "Write" transform that has side effects.
+        # pylint: disable=expression-not-assigned
+        output | 'Write' >> WriteToText(known_args.output)
 
 
 if __name__ == '__main__':
